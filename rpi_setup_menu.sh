c7d15c282fd71d4dd5249c7530ad08ffbc24603f
cms66 <58144229+cms66@users.noreply.github.com>
2019-11-30 20:48:37 +0000

    Add files via upload

---
+++ rpi_setup_menu.sh
@@ -0,0 +1,30 @@
+# Setup main menu
+# Check/Run other setup scripts
+
+show_main_menu()
+{
+	clear
+	echo "Setup Main menu \n----------"
+	echo "select setup option or x to exit \n 1) System \n 2) Hardware \n 3) NFS \n 4) SSH - Shared keys \n 5) OpenMPI \n 6) OpenCV \n 7) OpenCL \n 8) Test \n"
+}
+
+read -p "User Name: " usrname
+export usrname
+show_main_menu
+read -p "Select option or x to exit: " n
+
+while [ $n != "x" ]; do
+	case $n in
+	    1) sh /home/$usrname/local/src/shell/rpi_setup_system.sh;;
+	    2) sh /home/$usrname/local/src/shell/rpi_setup_hardware.sh;;
+	    3) sh /home/$usrname/local/src/shell/rpi_setup_nfs.sh;;
+	    4) sh /home/$usrname/local/src/shell/rpi_setup_ssh_keys.sh;;
+	    5) sh /home/$usrname/local/src/shell/rpi_setup_openmpi.sh;;
+	    6) sh /home/$usrname/local/src/shell/rpi_setup_opencv.sh;;
+	    7) sh /home/$usrname/local/src/shell/rpi_setup_opencl.sh;;
+	    8) sh /home/$usrname/local/src/shell/rpi_setup_test.sh;;
+	    *) read -p "invalid option - press enter to return to menu" errkey;;
+	esac
+	show_main_menu
+	read -p "Select option or x to exit: " n
+done
