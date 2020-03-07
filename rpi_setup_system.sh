359cdaefc3ee0235c8ed968608658b744715ca5d
cms66 <58144229+cms66@users.noreply.github.com>
2020-03-07 13:41:04 +0000

    Update rpi_setup_system.sh

--- rpi_setup_system.sh
+++ rpi_setup_system.sh
@@ -6,6 +6,6 @@ userdel -r -f pi
 apt-get -y update
 apt-get -y upgrade
 #  - System tools + Development
-apt-get -y install python python2.7-dev python3-dev python-pip python3-pip gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common
+apt-get -y install python python2.7-dev python3-dev python-pip python3-pip gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common fail2ban
 read -p "Finished System setup, press enter to return to menu" input
 
