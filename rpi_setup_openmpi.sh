446f0b6ad76d18297f1caa8386c66e36ef371276
cms66 <58144229+cms66@users.noreply.github.com>
2020-03-08 14:06:40 +0000

    Update rpi_setup_openmpi.sh

--- rpi_setup_openmpi.sh
+++ rpi_setup_openmpi.sh
@@ -1,4 +1,4 @@
-# OpenMPI 4.0.2
+# OpenMPI 4.0.3
 show_mpi_menu()
 {
 	clear
@@ -10,30 +10,30 @@ show_mpi_menu()
 install_local()
 {
 	cd /home/$usrname
-	wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.2.tar.gz
-	tar -xzf openmpi-4.0.2.tar.gz
-	cd openmpi-4.0.2
+	wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.3.tar.gz
+	tar -xzf openmpi-4.0.3.tar.gz
+	cd openmpi-4.0.3
 	./configure
 	make -j4 all
 	make install
 	ldconfig
 	cd /home/$usrname
-	rm -rf openmpi-4.0.2
-	rm -rf openmpi-4.0.2.tar.gz
+	rm -rf openmpi-4.0.3
+	rm -rf openmpi-4.0.3.tar.gz
 	mpirun --version
-	read -p "OpenMPI 4.0.2 - Local install finished, press enter to return to menu" input
+	read -p "OpenMPI 4.0.3 - Local install finished, press enter to return to menu" input
 }
 
 # 2- Build/install on server
 install_server()
 {
-	read -p "OpenMPI 4.0.2 - Server install TODO, press enter to return to menu" input
+	read -p "OpenMPI 4.0.3 - Server install TODO, press enter to return to menu" input
 }
 
 # 3- Install to run from server
 install_client()
 {
-	read -p "OpenMPI 4.0.2 - Client install TODO, press enter to return to menu" input
+	read -p "OpenMPI 4.0.3 - Client install TODO, press enter to return to menu" input
 }
 
 show_mpi_menu
