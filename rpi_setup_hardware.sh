ada34375936af2ff64486a81c8aa61974c017330
cms66 <58144229+cms66@users.noreply.github.com>
2020-12-13 09:50:42 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -61,7 +61,7 @@ setup_sense_hat()
 	read -p "Sense Hat setup done, it is recommended to calibrate the sense hat to improve accuracy, this can be done now or later from the Hardware menu. Press y to calibrate now or enter to return to menu" input
 	if [ X$input = X"y" ]
 	then
-		calib_sense_hat()
+		calib_sense_hat
 	else
 	fi
 }
