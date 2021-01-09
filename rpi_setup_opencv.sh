2cae8e8db2858ed7813fc8a6042e36e1c658df46
cms66 <58144229+cms66@users.noreply.github.com>
2021-01-09 17:35:47 +0000

    Update rpi_setup_opencv.sh

--- rpi_setup_opencv.sh
+++ rpi_setup_opencv.sh
@@ -14,8 +14,7 @@ mkdir /home/$usrname/opencv-4.5.1/build
 cd /home/$usrname/opencv-4.5.1/build
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=OFF -D ENABLE_NEON=ON -D ENABLE_VFPV3=ON -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_EXTRA_MODULES_PATH=/home/$usrname/opencv_contrib-4.5.1/modules -D BUILD_EXAMPLES=OFF ..
 sed -i "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g" /etc/dphys-swapfile
-/etc/init.d/dphys-swapfile stop
-/etc/init.d/dphys-swapfile start
+/etc/init.d/dphys-swapfile restart
 cores=$(nproc)
 if [ $cores -gt 1 ]
 then
@@ -27,8 +26,7 @@ make -j$cores
 make install
 ldconfig
 sed -i "s/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g" /etc/dphys-swapfile
-/etc/init.d/dphys-swapfile stop
-/etc/init.d/dphys-swapfile start
+/etc/init.d/dphys-swapfile restart
 cd /home/$usrname
 rm -rf opencv*
 read -p "OpenCV 4.5.1 - install finished, press enter to return to menu" input
