5d8d3ebe00ecd39eecd9a96bdaeabe558d3d3258
cms66 <58144229+cms66@users.noreply.github.com>
2021-01-26 22:27:07 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -7,7 +7,7 @@ show_hardware_menu()
 {
 	clear
 	printf "Hardware setup menu \n----------"
-	printf "select setup option or x to exit \n 1) CSI Camera \n 2) USB Camera \n 3) Sense Hat \n 4) Calibrate Sense Hat \n 5) Arduino - USB \n 6) Arduino I2C \n 7) GPS \n 8) TFT LCD \n 9) Calibrate display \n 10) Bluetooth \n 11) Robotic arm \n 12) DVB TV Hat \n 13) Audio (for Desktop image) \n 14) Arduino Libraries \n"
+	printf "select setup option or x to exit \n 1) CSI Camera \n 2) USB Camera \n 3) Sense Hat \n 4) Calibrate Sense Hat \n 5) Arduino - USB \n 6) Arduino I2C \n 7) GPS \n 8) TFT LCD \n 9) Calibrate display \n 10) Bluetooth \n 11) Robotic arm \n 12) DVB TV Hat \n 13) Audio (for Desktop image) \n 14) Arduino Libraries \n 15) Pressure sensor \n"
 }
 
 # 1 CSI Camera - Works - requires reboot
@@ -150,7 +150,7 @@ setup_audio_desktop()
 	read -p "Audio setup done, press enter to return to menu" input
 }
 
-# 1 Arduino Libraries
+# 14 Arduino Libraries
 setup_arduino_libs()
 {
 	# Install libraries to /usr/share/arduino/libraries/
@@ -171,6 +171,11 @@ setup_arduino_libs()
 	read -p "Arduino Libraries setup done, press enter to return to menu" input
 }
 
+# 15 Pressure sensor (MPL3115A2)
+setup_pressure_sensor()
+{
+	read -p "Pressure sensor setup TODO, press enter to return to menu" input
+}
 show_hardware_menu
 read -p "Select option or x to exit to main menu: " n
 while [ $n != "x" ]; do
@@ -189,6 +194,7 @@ while [ $n != "x" ]; do
 		12) setup_dvb_tv;;
 		13) setup_audio_desktop;;
 		14) setup_arduino_libs;;
+		15) setup_pressure_sensor;;
 		*) read -p "invalid option - press enter to continue" errkey;;
 	esac
 	show_hardware_menu
