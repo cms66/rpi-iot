82e4b8b8f89588ad597a9e15de5974d9bcb91e7e
cms66 <58144229+cms66@users.noreply.github.com>
2021-02-07 14:34:42 +0000

    Update rpi_setup_system.sh

--- rpi_setup_system.sh
+++ rpi_setup_system.sh
@@ -12,18 +12,16 @@ chattr +i /etc/apt/trusted.gpg.d/microsoft.gpg
 apt-get -y update
 apt-get -y upgrade
 #  - System tools + Development
-apt-get -y install python python2.7-dev python3-dev python-pip python3-pip gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common
-# TODO - add security
-# apt-get -y install fail2ban ufw
+apt-get -y install python python2.7-dev python3-dev python-pip python3-pip gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common fail2ban ufw
 
 # Configure firewall (ufw)
-#ufw allow 22
-#ufw logging on
-#ufw enable
+ufw allow 22
+ufw logging on
+ufw enable
 
 # Configure fail2ban
-#cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-#cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 read -p "Finished System setup, press enter to return to menu" input
 
