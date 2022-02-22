65491e44935ea4ee3fa1ecce466da1fe688a73ab
cms66 <58144229+cms66@users.noreply.github.com>
2022-02-22 21:19:48 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -16,12 +16,10 @@ show_hardware_menu()
 # 1 CSI Camera - Works - requires reboot
 setup_cam_csi()
 {
-	# Enable Camera
-	echo "start_x=1" >> /boot/config.txt
 	# grant access to camera for video group + add user to group
 	echo 'SUBSYSTEM=="vchiq",GROUP="video",MODE="0660"' > /etc/udev/rules.d/10-vchiq-permissions.rules
 	usermod -a -G video $usrname
-	apt-get -y install python-picamera python3-picamera v4l-utils
+	#apt-get -y install python-picamera python3-picamera v4l-utils
 	# Set firewall rule - assumes pinodeX numbering with port 8080 + node number e.g pinode1 = port 8081
 	hname=$(hostname)
 	fport=$(((${hname//[^0-9]/}) + 8080))
