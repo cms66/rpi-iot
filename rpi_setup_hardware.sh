4933daca7acd5fb3c841478b6d47d2b080ae3591
cms66 <58144229+cms66@users.noreply.github.com>
2022-03-05 16:30:40 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -16,10 +16,13 @@ show_hardware_menu()
 # 1 CSI Camera - Works - requires reboot
 setup_cam_csi()
 {
+	# Enable legacy camera
+	sed -i 's/camera_auto_detect=1/#camera_auto_detect=1/g' /boot/config.txt
+	echo "start_x=1" >> /boot/config.txt
 	# grant access to camera for video group + add user to group
 	echo 'SUBSYSTEM=="vchiq",GROUP="video",MODE="0660"' > /etc/udev/rules.d/10-vchiq-permissions.rules
 	usermod -a -G video $usrname
-	apt-get -y install v4l-utils
+	apt-get -y install v4l-utils python3-picamera
 	# Set firewall rule - assumes pinodeX numbering with port 8080 + node number e.g pinode1 = port 8081
 	#hname=$(hostname)
 	#fport=$(((${hname//[^0-9]/}) + 8080))
@@ -76,9 +79,7 @@ calib_sense_hat()
 setup_arduino_usb()
 {
 	usermod -a -G dialout $usrname
-	pip install pyserial
-	pip3 install pyserial
-	apt-get -y install arduino-core arduino-mk
+	apt-get -y install arduino-core-avr arduino-cli avrdude pyserial
 	read -p "Arduino USB setup done, press enter to return to menu" input
 }
 
