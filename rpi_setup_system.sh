7807ab318e2d56e616544a449eb3ea99e017705c
cms66 <58144229+cms66@users.noreply.github.com>
2024-01-03 14:04:11 +0000

    Update rpi_setup_system.sh

--- rpi_setup_system.sh
+++ rpi_setup_system.sh
@@ -1,13 +1,5 @@
 # Login as created user and sudo run
-# Remove default user pi
-userdel -r -f pi
 # Install software
-# Disable Microsoft Repo
-echo "0.0.0.0 packages.microsoft.com" >> /etc/hosts
-apt-mark hold raspberrypi-sys-mods
-rm -vf /etc/apt/trusted.gpg.d/microsoft.gpg
-touch /etc/apt/trusted.gpg.d/microsoft.gpg
-chattr +i /etc/apt/trusted.gpg.d/microsoft.gpg
 # - Update
 apt-get -y update
 apt-get -y upgrade
