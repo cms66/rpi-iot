486360690b1b1cdf3e8cc5926302aab6b0cb22ee
cms66 <58144229+cms66@users.noreply.github.com>
2024-01-14 16:41:02 +0000

    Update rpi_setup_base.sh

--- rpi_setup_base.sh
+++ rpi_setup_base.sh
@@ -8,7 +8,7 @@
 
 read -p "Username for setup (you are running as root): " usrname
 # create local folder structure for created user with code examples
-tar -xvzf local.tgz -C /home/$usrname
+tar -xvzf ./local.tgz -C /home/$usrname
 rm local.tgz
 # copy build scripts to local folder + set owner to created user
 mv ./rpi_*.sh /home/$usrname/local/src/shell
@@ -16,24 +16,24 @@ mv ./nfs-export.tgz /home/$usrname/local/src/shell
 chown -R $usrname:$usrname /home/$usrname/local/
 
 # Networking
-piname=$(hostname)
-echo "127.0.0.1   $piname.local $piname" >> /etc/hosts
-localip=$(hostname -I | awk '{print $1}')
-echo "$localip   $piname.local $piname" >> /etc/hosts
-sed -i "s/rootwait/rootwait ipv6.disable=1/g" /boot/cmdline.txt
+#piname=$(hostname)
+#echo "127.0.0.1   $piname.local $piname" >> /etc/hosts
+#localip=$(hostname -I | awk '{print $1}')
+#echo "$localip   $piname.local $piname" >> /etc/hosts
+#sed -i "s/rootwait/rootwait ipv6.disable=1/g" /boot/cmdline.txt
 
 # Disable root SSH login
-sed -i 's/#PermitRootLogin\ prohibit-password/PermitRootLogin\ no/g' /etc/ssh/sshd_config
+#sed -i 's/#PermitRootLogin\ prohibit-password/PermitRootLogin\ no/g' /etc/ssh/sshd_config
 
 # Set default shell to bash
-echo "dash dash/sh boolean false" | debconf-set-selections
-DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
+#echo "dash dash/sh boolean false" | debconf-set-selections
+#DEBIAN_FRONTEND=noninteractive dpkg-reconfigure dash
 
 # Reboot or Poweroff (if static IP setup needed on router)
-read -p "Finished base setup, press p to poweroff (if setting a static IP on router) or any other key to reboot, then login as $usrname: " input
-if [ X$input = X"p" ]
-then
-	poweroff
-else
-	reboot
-fi
+#read -p "Finished base setup, press p to poweroff (if setting a static IP on router) or any other key to reboot, then login as $usrname: " input
+#if [ X$input = X"p" ]
+#then
+#	poweroff
+#else
+#	reboot
+#fi
