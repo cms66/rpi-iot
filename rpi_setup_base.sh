211f03b8ccba5b4e6a656f5ef76e87cd1e5c8623
cms66 <58144229+cms66@users.noreply.github.com>
2024-01-27 18:17:54 +0000

    Update rpi_setup_base.sh

--- rpi_setup_base.sh
+++ rpi_setup_base.sh
@@ -11,6 +11,7 @@ tar -xvzf /boot/firmware/rpi-iot-master/local.tgz -C /home/$usrname
 # Move build scripts to hidden local folder
 mkdir /home/$usrname/.pisetup
 mv /boot/firmware/rpi-iot-master/* /home/$usrname/.pisetup
+rm -R /boot/firmware/rpi-iot-master
 chown -R $usrname:$usrname /home/$usrname/.pisetup
 chown -R $usrname:$usrname /home/$usrname/local/
 # Add bash alias for setup and test menu
