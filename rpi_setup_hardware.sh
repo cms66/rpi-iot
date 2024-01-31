3b5ae71639593c7e6dc640e991fdf1fd21130eb2
cms66 <58144229+cms66@users.noreply.github.com>
2024-01-31 15:00:45 +0000

    Update rpi_setup_hardware.sh

--- rpi_setup_hardware.sh
+++ rpi_setup_hardware.sh
@@ -10,7 +10,7 @@ show_hardware_menu()
 {
 	clear
 	printf "Hardware setup menu \n----------"
-	printf "select setup option or x to exit \n 1) CSI Camera \n 2) USB Camera \n 3) Sense Hat \n 4) Calibrate Sense Hat \n 5) Arduino - USB \n 6) Arduino I2C \n 7) GPS \n 8) TFT LCD \n 9) Calibrate display \n 10) Bluetooth \n 11) Robotic arm \n 12) DVB TV Hat \n 13) Audio (for Desktop image) \n 14) Arduino Libraries \n 15) Pressure sensor \n 16) Setup I2C \n"
+	printf "select setup option or x to exit \n 1) CSI Camera \n 2) USB Camera \n 3) Sense Hat \n 4) Calibrate Sense Hat \n 5) Arduino - USB \n 6) Arduino I2C \n 7) GPS \n 8) TFT LCD \n 9) Calibrate display \n 10) Bluetooth \n 11) Robotic arm \n 12) DVB TV Hat \n 13) Audio (for Desktop image) \n 14) Arduino Libraries \n 15) Pressure sensor \n 16) Setup I2C \n 16) Setup GPS - PA1010D \n"
 }
 
 # 1 CSI Camera - Works - requires reboot
